f40c2baa0c0dcc9fb367b45dedfcca11a5cdd9d8
Peter Lofgren <plofgren@cs.stanford.edu>
2014-04-16 09:11:41 -0700

    Implmented and tested frontier computation.

--- src/main/scala/FastPPRConfiguration.scala
+++ src/main/scala/FastPPRConfiguration.scala
@@ -1,14 +1,19 @@
 case class FastPPRConfiguration(
-                                 //delta: Double, // Minimum value of pi(s,t) for which Fast-PPR is accurate
-                                 //pFail: Double, // The probability that the Monte Carlo walks introduce too much error
-                                 nWalksConstant : Double, // In forward work, we will do nWalksConstant / epsilon_f walks
-                                 beta: Double, // In reverse PPR computation, our additive threshold is beta * epsilon_r
-                                 deltaTimesN: Double // delta, the minimum ppr value we reliably detect, is deltaTimesN / (nNodes)
+                                 alpha: Float, // teleport probability
+                                 //delta: Float, // Minimum value of pi(s,t) for which Fast-PPR is accurate
+                                 //pFail: Float, // The probability that the Monte Carlo walks introduce too much error
+                                 nWalksConstant : Float, // In forward work, we will do nWalksConstant / epsilon_f walks
+                                 beta: Float, // In reverse PPR computation, our additive bound is beta * epsilon_r
+                                 deltaTimesN: Float // delta, the minimum ppr value we reliably detect, is deltaTimesN / (nodeCount)
                                  )         {
-  def nWalks(epsilonF: Double): Int = (nWalksConstant / epsilonF).toInt
-  def delta(nNodes: Int): Double = deltaTimesN / nNodes
+  def nWalks(epsilonF: Float): Int = (nWalksConstant / epsilonF).toInt
+  def delta(nodeCount: Int): Float = deltaTimesN / nodeCount
 }
 
 object FastPPRConfiguration {
-  val DefaultConfiguration = FastPPRConfiguration(24 *  math.log(1.0e6) , 1.0  / 6.0, 4.0)
+  val defaultConfiguration = FastPPRConfiguration(
+    alpha=0.2f,
+    nWalksConstant=24 *  math.log(1.0e6).toFloat,
+    beta=1.0f  / 6.0f,
+    deltaTimesN=4.0f)
 }
