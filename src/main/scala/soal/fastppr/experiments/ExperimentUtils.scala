c7f03d9df93490dca65d8271bc6fa61aa6646ae9
Peter Lofgren <plofgren@cs.stanford.edu>
2014-04-23 16:34:23 -0700

    Runtime Experiments

---
+++ src/main/scala/soal/fastppr/experiments/ExperimentUtils.scala
@@ -0,0 +1,42 @@
+package soal.fastppr.experiments
+
+import com.twitter.cassovary.graph.{Node, StoredGraphDir, DirectedGraph}
+import com.twitter.cassovary.util.io.AdjacencyListGraphReader
+import com.twitter.cassovary.graph.StoredGraphDir._
+import com.twitter.cassovary.graph.StoredGraphDir.StoredGraphDir
+import scala.util.Random
+import soal.fastppr.FastPPRConfiguration
+
+object ExperimentUtils {
+  def readGraph(args: Array[String]): DirectedGraph = {
+    assert(args.length == 1)
+    val filenameStart = args(0).lastIndexOf('/') + 1
+    val graphDirectory = args(0).take(filenameStart)
+    val graphFilename = args(0).drop(filenameStart)
+    println("graph:" + graphFilename)
+
+    val reader = new AdjacencyListGraphReader(graphDirectory, graphFilename) {
+      override def storedGraphDir: StoredGraphDir = StoredGraphDir.BothInOut
+    }
+    val graph = reader.toArrayBasedDirectedGraph()
+    graph
+  }
+
+  def randomElement[A](xs: Seq[A]): A = {
+    xs(Random.nextInt(xs.size))
+  }
+
+  /* pass in Node sequence of graph because graph doesn't seem to have random access to nodes */
+  def sampleFromPageRank(graph: DirectedGraph, nodes: Seq[Node], config: FastPPRConfiguration): Node = {
+    var currentNode = randomElement(nodes)
+    while(Random.nextFloat() > config.teleportProbability) {
+      currentNode = currentNode.randomOutboundNode match {
+        case Some(uId) => graph.getNodeById(uId).get
+        case None  => randomElement(nodes) // Teleport away from sink nodes
+      }
+    }
+    currentNode
+  }
+
+  def mean(xs: Seq[Float]): Float = xs.sum / xs.size
+}
